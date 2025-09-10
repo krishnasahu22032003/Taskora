f9fa5c18e643aa0cdcebe3db43ddef3e1cb6b3db
krishna sahu <krishna.sahu.work@gmail.com>
2025-09-10 23:10:41 +0530

    frontend updated code

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,14 +1,39 @@
+import { Routes, useNavigate } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import { useEffect, useState } from "react"
 
 
 function App() {
-
-
+const navigate = useNavigate()
+const [currentUser , setcurrentUser]=useState(()=>{ 
+  const stored = localStorage.getItem('currentUser')
+  return stored ? JSON.parse(stored) : null
+})
+useEffect(()=>{
+  if(currentUser){
+    localStorage.setItem('currentUser',JSON.stringify(currentUser))
+  }else{
+    localStorage.removeItem('currentUser')
+  }
+},[currentUser])
+const handleAuthSubmit = data => {
+  const user ={
+    email:data.email,
+    name:data.name || 'User',
+    avatar:''
+  }
+  setcurrentUser(user)
+  navigate('/',{replace:true})
+}
+const handleLogout = ()=>{
+  localStorage.removeItem('token')
+  setcurrentUser(null)
+  navigate('\login',{replace:true})
+}
   return (
-    <>
-   <div className="bg-red-300 text-black">
-    hello hi there
-   </div>
-    </>
+<Routes>
+<Route path='/' element={}/>
+</Routes>
   )
 }
 
